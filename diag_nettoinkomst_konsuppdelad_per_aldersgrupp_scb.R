2e8661d29332a37ae8d9d95aa1261da4c99cc4c4
Peter Möller <larspetermoller@gmail.com>
2024-08-22 14:31:35 +0200

    Uppdateringar.

--- diag_nettoinkomst_konsuppdelad_per_aldersgrupp_scb.R
+++ diag_nettoinkomst_konsuppdelad_per_aldersgrupp_scb.R
@@ -16,17 +16,17 @@ diag_nettoinkomst_kon_aldersgrupp_scb <- function(
     diag_valt_ar_kon_diff_aldersgrupper = TRUE           # diagram med diff i medianinkomst mellan män och kvinnor för valt år, åldersgrupper på x-axeln
     ){
 
-# om parametern demo är satt till TRUE så öppnas en flik i webbläsaren med ett exempel på hur diagrammet ser ut och därefter avslutas funktionen
-# demofilen måste läggas upp på webben för att kunna öppnas, vi lägger den på Region Dalarnas github-repo som heter utskrivna_diagram
-if (demo){
-  demo_url <- 
-c("https://region-dalarna.github.io/utskrivna_diagram/diff_medianinkomst_Dalarna_2022.png",
-"https://region-dalarna.github.io/utskrivna_diagram/diff_medianinkomst_Dalarna_ar2000_2022.png",
-"https://region-dalarna.github.io/utskrivna_diagram/nettoinkomst_Dalarna_ar2000_2022.png")
-  walk(demo_url, ~browseURL(.x))
-  if (length(demo_url) > 1) cat(paste0(length(demo_url), " diagram har öppnats i webbläsaren."))
-  stop_tyst()
-}
+  # om parametern demo är satt till TRUE så öppnas en flik i webbläsaren med ett exempel på hur diagrammet ser ut och därefter avslutas funktionen
+  # demofilen måste läggas upp på webben för att kunna öppnas, vi lägger den på Region Dalarnas github-repo som heter utskrivna_diagram
+  if (demo){
+    demo_url <- 
+  c("https://region-dalarna.github.io/utskrivna_diagram/diff_medianinkomst_Dalarna_2022.png",
+  "https://region-dalarna.github.io/utskrivna_diagram/diff_medianinkomst_Dalarna_ar2000_2022.png",
+  "https://region-dalarna.github.io/utskrivna_diagram/nettoinkomst_Dalarna_ar2000_2022.png")
+    walk(demo_url, ~browseURL(.x))
+    if (length(demo_url) > 1) cat(paste0(length(demo_url), " diagram har öppnats i webbläsaren."))
+    stop_tyst()
+  }
 
   if (!require("pacman")) install.packages("pacman")
   p_load(tidyverse,
