fb543e6ac42289a3f32a307b832a1f0461be1925
FaluPeppe <larspetermoller@gmail.com>
2025-01-26 12:07:27 +0100

    Test buggfix diagramskript om bef utfall prognos.

--- diag_sjalvskattad_halsa_kon_lan_fohm.R
+++ diag_sjalvskattad_halsa_kon_lan_fohm.R
@@ -16,8 +16,8 @@ diag_sjalvskattad_halsa_kon_lan <- function(
     kortnamn_lan = TRUE,                    # TRUE så tas " län" bort ur länsnamnet
     diagram_0_till_100_procent = TRUE,       # TRUE så går skalan alltid från 0 till 100 procent, annars anpassas skalan efter data
     logga_path = NA                         # NULL för att köra utan logga             
-  ) {
-
+) {
+  
   # ===============================================================================================
   #
   # Diagram för att skriva ut självskattad hälsa från Hälsa på lika villkor-enkäten hos 
@@ -30,7 +30,7 @@ diag_sjalvskattad_halsa_kon_lan <- function(
   
   if (!require("pacman")) install.packages("pacman")
   p_load(tidyverse,
-     			glue)
+         glue)
   
   source("https://raw.githubusercontent.com/Region-Dalarna/hamta_data/main/hamta_sjalvskattad_halsa_region_halsotillstand_andel_och_konfidensintervall_kon_ar_hlv1allmxreg_fohm.R")
   source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_SkapaDiagram.R", encoding = "utf-8")
@@ -44,7 +44,7 @@ diag_sjalvskattad_halsa_kon_lan <- function(
       stop("Ingen output-mapp angiven, kör funktionen igen och ge parametern output-mapp ett värde.")
     }
   }
-
+  
   # om ingen färgvektor är medskickad, kolla om funktionen diagramfärger finns, annars använd r:s defaultfärger
   if (all(is.na(diagram_fargvekt))) {
     if (exists("diagramfarger", mode = "function")) {
@@ -53,18 +53,18 @@ diag_sjalvskattad_halsa_kon_lan <- function(
       diagram_fargvekt <- hue_pal()(9)
     }
   }
-
+  
   gg_list <- list()
   
   sjalvskattad_halsa_df <- hamta_sjalvskattad_halsa_region_halsotillstand_andel_och_konfidensintervall_kon_ar_fohm(
-  			region_vekt = region_vekt,			   # Val av region. Finns: "00", "01", "03", "04", "05", "06", "07", "08", "09", "10", "12", "13", "14", "17", "18", "19", "20", "21", "22", "23", "24", "25"
-  			halsotillstand_klartext = svarsalterantiv_klartext,			 #  Finns: "Bra eller mycket bra hälsa", "Dålig eller mycket dålig hälsa", "Långvarig sjukdom", "Nöjd med sexlivet"
-  			andel_och_konfidensintervall_klartext = andel_konfinterv_klartext,			 #  Finns: "Andel", "Konfidensintervall nedre gräns", "Konfidensintervall övre gräns", "Antal svar"
-  			kon_klartext =kon_klartext,			 #  NA = tas inte med i uttaget,  Finns: "Totalt", "Kvinnor", "Män"
-  			tid_koder = tid_koder,			 # "*" = alla år eller månader, "9999" = senaste, finns: "2004-2007", "2005-2008", "2006-2009", "2007-2010", "2008-2011", "2009-2012", "2010-2013", "2011-2014", "2012-2015", "2013-2016", "2015-2018", "2017-2020", "2018-2021", "2019-2022", "2021-2024"
-  			output_mapp = output_mapp,			# anges om man vill exportera en excelfil med uttaget, den mapp man vill spara excelfilen till
-  			excel_filnamn = "test_fohm.xlsx",			# filnamn för excelfil som exporteras om excel_filnamn och output_mapp anges
-  			returnera_df = TRUE			# TRUE om man vill ha en dataframe i retur från funktionen
+    region_vekt = region_vekt,			   # Val av region. Finns: "00", "01", "03", "04", "05", "06", "07", "08", "09", "10", "12", "13", "14", "17", "18", "19", "20", "21", "22", "23", "24", "25"
+    halsotillstand_klartext = svarsalterantiv_klartext,			 #  Finns: "Bra eller mycket bra hälsa", "Dålig eller mycket dålig hälsa", "Långvarig sjukdom", "Nöjd med sexlivet"
+    andel_och_konfidensintervall_klartext = andel_konfinterv_klartext,			 #  Finns: "Andel", "Konfidensintervall nedre gräns", "Konfidensintervall övre gräns", "Antal svar"
+    kon_klartext =kon_klartext,			 #  NA = tas inte med i uttaget,  Finns: "Totalt", "Kvinnor", "Män"
+    tid_koder = tid_koder,			 # "*" = alla år eller månader, "9999" = senaste, finns: "2004-2007", "2005-2008", "2006-2009", "2007-2010", "2008-2011", "2009-2012", "2010-2013", "2011-2014", "2012-2015", "2013-2016", "2015-2018", "2017-2020", "2018-2021", "2019-2022", "2021-2024"
+    output_mapp = output_mapp,			# anges om man vill exportera en excelfil med uttaget, den mapp man vill spara excelfilen till
+    excel_filnamn = "test_fohm.xlsx",			# filnamn för excelfil som exporteras om excel_filnamn och output_mapp anges
+    returnera_df = TRUE			# TRUE om man vill ha en dataframe i retur från funktionen
   )
   
   if (kortnamn_lan) sjalvskattad_halsa_df <- sjalvskattad_halsa_df %>% mutate(region = region %>% skapa_kortnamn_lan())
@@ -101,32 +101,32 @@ diag_sjalvskattad_halsa_kon_lan <- function(
   flera_regioner <- if(length(unique(sjalvskattad_halsa_df$region)) > 1) TRUE else FALSE
   
   gg_obj <- SkapaStapelDiagram(skickad_df = sjalvskattad_halsa_df,
-  			 skickad_x_var = if (flera_ar) "År" else "region",
-  			 skickad_y_var = "Fysisk hälsa, allmänt hälsotillstånd efter region kön och år",
-  			 skickad_x_grupp = if (konsuppdelat) "Kön" else NA,
-  			 diagram_titel = diagramtitel,
-  			 utan_diagramtitel = ta_bort_diagramtitel,
-  			 diagram_capt = diagram_capt,
-  			 stodlinjer_avrunda_fem = TRUE,
-  			 filnamn_diagram = diagramfil,
-  			 dataetiketter = visa_dataetiketter,
-  			 manual_y_axis_title = "procent",
-  			 procent_0_100_10intervaller = diagram_0_till_100_procent,
-  			 x_axis_lutning = if (flera_ar) 45 else 0,
-  			 manual_x_axis_text_vjust = if (flera_ar) 1 else 0,
-  			 manual_x_axis_text_hjust = if (flera_ar) 1 else 0.5,
-  			 manual_color = diagram_fargvekt,
-  			 lagg_pa_logga = if (is.null(logga_path)) FALSE else TRUE,
-  			 logga_path = logga_path,
-  			 output_mapp = output_mapp,
-  			 diagram_facet = if (flera_ar & flera_regioner) TRUE else FALSE,
-  			 facet_grp = if (flera_ar & flera_regioner) "region" else NA,
-  			 facet_scale = "free",
-  			 facet_legend_bottom = if (konsuppdelat) TRUE else FALSE
+                               skickad_x_var = if (flera_ar) "År" else "region",
+                               skickad_y_var = "Fysisk hälsa, allmänt hälsotillstånd efter region kön och år",
+                               skickad_x_grupp = if (konsuppdelat) "Kön" else NA,
+                               diagram_titel = diagramtitel,
+                               utan_diagramtitel = ta_bort_diagramtitel,
+                               diagram_capt = diagram_capt,
+                               stodlinjer_avrunda_fem = TRUE,
+                               filnamn_diagram = diagramfil,
+                               dataetiketter = visa_dataetiketter,
+                               manual_y_axis_title = "procent",
+                               procent_0_100_10intervaller = diagram_0_till_100_procent,
+                               x_axis_lutning = if (flera_ar) 45 else 0,
+                               manual_x_axis_text_vjust = if (flera_ar) 1 else 0,
+                               manual_x_axis_text_hjust = if (flera_ar) 1 else 0.5,
+                               manual_color = diagram_fargvekt,
+                               lagg_pa_logga = if (is.null(logga_path)) FALSE else TRUE,
+                               logga_path = logga_path,
+                               output_mapp = output_mapp,
+                               diagram_facet = if (flera_ar & flera_regioner) TRUE else FALSE,
+                               facet_grp = if (flera_ar & flera_regioner) "region" else NA,
+                               facet_scale = "free",
+                               facet_legend_bottom = if (konsuppdelat) TRUE else FALSE
   )
   
   gg_list <- c(gg_list, list(gg_obj))
   names(gg_list)[[length(gg_list)]] <- diagramfil %>% str_remove(".png")
- 
+  
   return(gg_list) 
 }
