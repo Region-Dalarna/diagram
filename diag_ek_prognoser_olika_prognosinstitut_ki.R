a35f7be1707dcc80819236850f60e24b870cabe3
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2025-10-06 10:54:12 +0200

    Mindre ändring

--- diag_ek_prognoser_olika_prognosinstitut_ki.R
+++ diag_ek_prognoser_olika_prognosinstitut_ki.R
@@ -1,17 +1,15 @@
-
-
-
-
 diag_ekonomiska_prognoser_olika_progn_institut_ki <- function(vald_variabel = "BNP",                  # finns: "BNP", "Hushållens konsumtion", "Offentlig konsumtion", "Fasta bruttoinvesteringar", "Lagerinvesteringar., förändr. i proc. av BNP föreg. år", "Export", "Import", "Antal sysselsatta, 15-74 år (AKU)", "Arbetslöshet, procent av arbetskraften, 15-74 år (AKU)", "Timlön, totalt (konjunkturlönestatistiken)", "Konsumentprisindex (KPI), årsgenomsnitt", "KPI med fast bostadsränta (KPIF), årsgenomsnitt", "Real disponibel inkomst (nationalräkenskaperna)", "Styrränta, vid årets slut, procent**", "Offentligt finansiellt sparande, procent av BNP", "Bytesbalans, procent av BNP (nationalräkenskaperna)", "Timlön, näringslivet (konjunkturlönestatistiken)"
                                                               valda_prognos_ar = "+1",                  # NA eller "*" = alla år, "+1" = aktuellt år + ett år, kan vara andra antal år 
                                                               endast_mest_aktuell_prognos = TRUE,      # TRUE om man bara vill ha den mest aktuella prognosen varje år, annars kommer alla prognoser som institut har gjort för prognosåret med i datasetet
                                                               output_mapp = utskriftsmapp(),
+                                                              x_axis_lutning = 45, # Lutning på x-axelns text
                                                               manual_y_axis_title = "Prognosticerad tillväxt (%)", # Möjlighet att styra namn på y-axel. Tenderar att överlappa med x-axel med prognosticerad tillväxt.
                                                               diagram_capt = "Källa: Konjunkturinstitutet, bearbetning av Samhällsanalys, Region Dalarna",
                                                               skriv_diagramfil = TRUE
                                                               ) { 
   
   # GG-list skapades inte. Lagt till nedan Jon 2025-10-06
+  # Har dessutom lagt till så att man kan ändra diverse parametrar i figuren
   
   if (!require("pacman")) install.packages("pacman")
   p_load(tidyverse)
@@ -51,6 +49,7 @@ diag_ekonomiska_prognoser_olika_progn_institut_ki <- function(vald_variabel = "B
                                manual_x_axis_text_vjust = 1,
                                manual_x_axis_text_hjust = 1,
                                manual_y_axis_title = manual_y_axis_title,
+                               x_axis_lutning = x_axis_lutning,
                                diagram_facet = TRUE,
                                facet_grp = "prognos_for_ar",
                                facet_scale = "free_x",
